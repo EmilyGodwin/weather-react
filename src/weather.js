f3e5c6872111c34e675fda5ac7d9f11a028c54d3
EmilyGodwin <godwie20@gmail.com>
2022-07-09 17:01:48 -0500

    Integrated weather api

--- src/weather.js
+++ src/weather.js
@@ -1,104 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div>
-      <div className="container">
-        <div className="box firstBox">
-          <div className="topRow row">
-            <div className="col">
-              <span className="currentDay" id="date">
-                Monday
-              </span>
-            </div>
-            <div className="col">
-              <span>
-                <form className="searchBar" id="search-form">
-                  <input
-                    className="searchInput"
-                    type="search"
-                    placeholder=" Enter City..."
-                    name="search"
-                    id="city-input"
-                    autoComplete="off"
-                  />
-                  <input type="submit" value="Search" className="button" />
-                </form>
-              </span>
-            </div>
-          </div>
-          <div className="rowTwo row">
-            <div className="col">
-              <div className="currentIcon">
-                <i className="fa-solid fa-sun"></i>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      date: "Monday",
+      time: "2:30 pm",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div>
+        <div className="container">
+          <div className="box firstBox">
+            <div className="topRow row">
+              <div className="col">
+                <span className="currentDay">{weatherData.date}</span>
               </div>
-              <button className="currentTemp temp" id="temperature">
-                68°F
+              <div className="col">
+                <span>
+                  <form className="searchBar" id="search-form">
+                    <input
+                      className="searchInput"
+                      type="search"
+                      placeholder=" Enter City..."
+                      name="search"
+                      id="city-input"
+                      autoComplete="off"
+                    />
+                    <input type="submit" value="Search" className="button" />
+                  </form>
+                </span>
+              </div>
+            </div>
+            <div className="rowTwo row">
+              <div className="col">
+                <div className="currentIcon">
+                  <i className="fa-solid fa-sun"></i>
+                </div>
+                <button className="currentTemp temp">
+                  {Math.round(weatherData.temperature)} °F
+                </button>
+              </div>
+              <div className="col">
+                <span>
+                  <div className="currentCity">{weatherData.city}</div>
+                  <ul>
+                    <li>
+                      Humidity: <span>{weatherData.humidity}</span>%
+                    </li>
+                    <li>
+                      Wind: <span>{weatherData.wind}</span> mph
+                    </li>
+                  </ul>
+                </span>
+              </div>
+              <div className="col">
+                <div className="clock">{weatherData.time}</div>
+                <div className="conditions">{weatherData.description}</div>
+              </div>
+            </div>
+            <div className="bottomRow row">
+              <button type="button" className="city col rounded">
+                London
+              </button>
+              <button type="button" className="city col rounded">
+                Macau
+              </button>
+              <button type="button" className="city col rounded">
+                Singapore
+              </button>
+              <button type="button" className="city col rounded">
+                Paris
               </button>
             </div>
-            <div className="col">
-              <span>
-                <div className="currentCity" id="city">
-                  Madison
-                </div>
-                <ul>
-                  <li>
-                    Humidity: <span id="humidity"></span>%
-                  </li>
-                  <li>
-                    Wind: <span id="wind"></span> mph
-                  </li>
-                </ul>
-              </span>
-            </div>
-            <div className="col">
-              <div className="clock" id="time">
-                2:30 pm
-              </div>
-              <div className="conditions" id="description">
-                Overcast Clouds
-              </div>
-            </div>
           </div>
-          <div className="bottomRow row">
-            <button type="button" className="city col rounded" id="london">
-              London
-            </button>
-            <button type="button" className="city col rounded" id="macau">
-              Macau
-            </button>
-            <button type="button" className="city col rounded" id="singapore">
-              Singapore
-            </button>
-            <button type="button" className="city col rounded" id="paris">
-              Paris
-            </button>
+          <div className="box secondBox">
+            <div className="weatherForecast"></div>
           </div>
         </div>
-        <div className="box secondBox">
-          <div className="weatherForecast" id="forecast"></div>
-        </div>
-      </div>
-      <footer className="link">
-        <a
-          href="https://github.com/EmilyGodwin/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        <span>
-          {" "}
-          by{" "}
+        <footer className="link">
           <a
-            href="https://emilygodwin.netlify.app"
+            href="https://github.com/EmilyGodwin/weather-react"
             target="_blank"
             rel="noreferrer"
           >
-            Emily Godwin
+            Open-source code
           </a>
-        </span>
-      </footer>
-    </div>
-  );
+          <span>
+            {" "}
+            by{" "}
+            <a
+              href="https://emilygodwin.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Emily Godwin
+            </a>
+          </span>
+        </footer>
+      </div>
+    );
+  } else {
+    const apiKey = "12e817575070bcd60eb64f87187b9c19";
+    let city = "Maple Bluff";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
